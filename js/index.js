04eb9ac29e6f69949ab1886a5c36ee127ed25327
ryancevb <141280376+ryancevb@users.noreply.github.com>
2023-08-20 22:13:56 -0300

    entrega 1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+  });
+});
+
+function logearse(e) {
+  if (!localStorage.getItem("correo") || !localStorage.getItem("pass")) {
+    window.location.href = "login.html";
+  }
+}
+
+logearse();
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "b") {
+    localStorage.clear();
+
+    alert("LocalStorage ha sido borrado.");
+  }
 });
---
+++ js/login.js
@@ -0,0 +1,53 @@
+document.getElementById("boton").addEventListener("click", validarRegistro);
+
+//Validar Registro
+function validarRegistro(e) {
+  let correo = document.getElementById("floatingInput3").value;
+  let contraseña = document.getElementById("exampleInputPassword1").value;
+  let alerta = document.getElementById("alerta");
+  const checkbox = document.getElementById("exampleCheck1");
+
+
+ let fallo = false;
+
+document.getElementById("obligatorio").style.display = "none";
+document.getElementById("obligatorio1").style.display = "none";
+document.getElementById("obligatorio2").style.display = "none";
+document.getElementById("alerta1").style.display = "none";
+
+  if(correo.indexOf("@") == -1){
+    document.getElementById("obligatorio").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+    fallo = true;
+  }
+  if(contraseña.length < 6){
+
+    document.getElementById("obligatorio1").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+    fallo = true;
+  }
+  if(!checkbox.checked){
+    document.getElementById("obligatorio2").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+    fallo = true;
+  }
+
+  if (!fallo) {
+    e.preventDefault();
+    localStorage.setItem("correo", correo);
+    localStorage.setItem("pass", contraseña);
+    window.location.href = "/index.html";
+  } 
+  }
+
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "b") {
+    localStorage.clear();
+
+    alert("LocalStorage ha sido borrado.");
+  }
+});
